3cc8968dd1a66a534272f7af5dd367e36189ec69
Samuel Chou <sam.chou@windystudios.com>
2013-05-26 00:34:12 -0700

    Add sprocket js that mirrors javascript load order in Flat UI Pro template

---
+++ vendor/assets/javascripts/flatuipro.js
@@ -0,0 +1,9 @@
+//= require jquery-ui-1.10.3.custom.min
+//= require jquery.ui.touch-punch.min
+//= require bootstrap-select
+//= require bootstrap-switch
+//= require flatui-checkbox
+//= require flatui-radio
+//= require jquery.tagsinput
+//= require jquery.placeholder
+//= require jquery.stacktable
